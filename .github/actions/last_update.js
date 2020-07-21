7b4df92f4a529d87c1bf21a88b1796642cfe6a31
Milan Somsedík <42219550+somsac@users.noreply.github.com>
2020-07-21 18:44:46 +0200

    Update last_update.js

--- .github/actions/last_update.js
+++ .github/actions/last_update.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var date = new Date();
 
-fs.writeFile('main/version.json', '{"last_update":"' + date.toISOString() + '"}', function (err) {
+fs.writeFile('version.json', '{"last_update":"' + date.toISOString() + '"}', function (err) {
   if (err) throw err;
   console.log('Replaced!');
 });
